c8856910532f3887365bcf20a22c515f9c70108e
lixiong <31425462+LiXiongGithub@users.noreply.github.com>
2018-05-16 17:13:04 +0800

    d
    d

--- src/main/java/com/lx/utils/CommonConstants.java
+++
@@ -1,8 +0,0 @@
-package com.lx.utils;
-
-public class CommonConstants {
-
-	public static String PATTERN_FULL = "yyyy-MM-dd HH:mm:ss";
-	public static String PATTERN_FULL_TRANSNO = "yyyyMMddHHmmss";
-	public static String PATTERN_SHORT = "yyyy-MM-dd";
-}
